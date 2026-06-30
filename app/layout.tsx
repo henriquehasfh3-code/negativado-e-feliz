e42cc75efef7d952bb423710fb6e38a833913f39
henriquehasfh3-code <henriquehasfh3@gmail.com>
2026-06-30 17:15:40 -0300

    feat: add google analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -133,6 +133,21 @@ export default function RootLayout({
               });
             `}
           </Script>
+
+          {/* Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-HXNDDGBBKY"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-HXNDDGBBKY');
+            `}
+          </Script>
         </ThemeProvider>
       </body>
     </html>
